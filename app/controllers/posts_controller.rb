44a62b64e75c49b31ac11b7ca035d9ba71b2f941
matthewpeak <matthewpea@gmail.com>
2020-03-16 11:16:00 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,15 @@ class PostsController < ApplicationController
   def edit
   end
 
+  
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+      if @post.update(post_params)
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
   end
+  
 
   private
 
